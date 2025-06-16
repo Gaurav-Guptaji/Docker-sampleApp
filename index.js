f2c5f200556048875d12587361a75be107a42842
Gaurav Gupta <lomagaurav@gmail.com>
2025-06-16 18:18:57 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require ('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res)=>res.send('Hello! This is a sample app using docker EC2 and github pipeline'));
+app.get('/', (req,res)=>res.send('Hello! This is a sample app using docker EC2 and github/jenkins pipeline'));
 app.listen(PORT,()=> console.log(`server is running on ${PORT}`));
